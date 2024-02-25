8cfc47d2d170a30e4f7692fcf613d71ef8046455
Adeeb Ahmed <adeebahmed1305@gmail.com>
2024-02-25 18:22:02 -0500

    env file edited

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        if message.content.startswith('~hello'):
+            await message.channel.send('Wassupppp')
+            
+    client.run(MTIxMTQzNTc4NTI3NTA1NjEyOA.Getgnn.BugDWp6IkeVz93oWqColG5rmgZKTc45vrfxwVM)
+        
+    
