87dda568adab1fa18e7254ee9690ceb0c80d5ee5
Adeeb Ahmed <adeebahmed1305@gmail.com>
2024-02-25 20:13:00 -0500

    Add AI features

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
-import discord
-from discord.ext import commands
+from typing import Final
 import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from userresponse import get_response
 
-client = commands.Bot(command_prefix='~', intents = discord.Intents.all())
+load_dotenv()
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
-@client.event
-async def on_ready():
-    print('I am {0.user}'.format(client) )
-    print("-----------------------------")
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents = intents)
+
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print("Message is empty because intents weren't enabled")
+        return
     
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
+    if is_private := user_message[0] == '~':
+        user_message=user_message[1:]
         
-        if message.content.startswith('~hello'):
-            await message.channel.send('Wassupppp')
-            
-    client.run(os.getenv('TOKEN'))
+    try:
+        response: str = get_response(user_message)
+        await message.channel.send(response) if is_private else await message.send(response)
+    except Exception as e:
+        print(e)
         
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is online!')
+    
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+    
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+    
+    print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+    
+def main() -> None:
+    client.run(token=TOKEN)
+    
+if __name__ == '__main__':
+    main()
+    
     
--- userresponse.py
+++ userresponse.py
@@ -1,2 +1,11 @@
+from random import choice, randint
+
 def get_response(user_input: str) -> str:
-    raise NotImplementedError('The code is missing!')
+    lowered: str = user_input.lower()
+    
+    if lowered == '':
+        return "What was that?"
+    elif 'hello' in lowered:
+        return "Yoooo what's up!"
+    else:
+        return "huh?"
