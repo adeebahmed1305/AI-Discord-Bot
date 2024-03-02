758d5a5b0c86699b363418b0719a7faefaad25be
Adeeb Ahmed <adeebahmed1305@gmail.com>
2024-03-02 18:24:30 -0500

    Project is now complete

--- main.py
+++ main.py
@@ -1,3 +1,61 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a5b83aa12589fbf14aae8f9fb309fff97d5af6f69b8f4e3c100dcbe861d2b7e0
-size 1779
+from typing import Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from userresponse import get_response
+from userresponse import chat_with_gpt
+
+load_dotenv() 
+TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents = intents)
+
+async def send_message(message: Message, user_message: str):
+    if not user_message:
+        print("Message is empty because intents weren't enabled")
+        return
+    
+    if is_private := user_message[0] == '~':
+        user_message=user_message[1:]
+        
+    try:
+        response: str = get_response(user_message)
+        await message.channel.send(response) if is_private else await message.send(response)
+    except Exception as e:
+        print(e)
+        
+@client.event
+async def on_ready():
+    print(f'{client.user} is online!')
+    
+@client.event
+async def on_message(message: Message):
+    if message.author == client.user:
+        return
+    
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+    
+    print(f'[{channel}] {username}: "{user_message}"')
+    await send_message(message, user_message)
+    
+    command, the_message = None, None
+    
+    for text in ['/ai', '/mr']:
+        if message.content.startswith(text):
+            command = message.content.split(' ')[0]
+            the_message = message.content.replace(text, '')
+            print(command, the_message)
+    
+    if command == '/ai' or command == '/mr':
+        bot_response = chat_with_gpt(prompt = the_message)
+        await message.channel.send(f"{bot_response}")
+        
+def main():
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main() 
--- userresponse.py
+++ userresponse.py
@@ -1,3 +1,35 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0b3b39b4542dfd7f3fcad245e9ae6d11d21869d7847bab79b843c3ccdc60f857
-size 960
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_response(user_input: str) -> str: 
+    lowered: str = user_input.lower()
+    
+    if lowered == '':
+        return "What was that?"
+    elif 'hello' in lowered:
+        return "Yoooo what's up!"
+    elif 'sakib is dumb' in lowered:
+        return "I know!"
+    elif ('how is your day?' or "How is your day" or "hows your day" or "how is your day" or "how's your day?" ) in lowered:
+        return "its going amazing, how's your's?"
+    elif 'bad' in lowered :
+        return "thats tuff"
+    elif 'good' in lowered:
+        return "good for you!"
+    else:
+        return "huh?"
+
+client = OpenAI(
+    api_key = os.getenv('CHATGPT_API_KEY')
+)
+
+def chat_with_gpt(prompt):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages = [{"role": "user", "content": prompt}]
+    )
+    
+    return response.choices[0].message.content.strip()
